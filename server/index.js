69310b89187f85f11e457cd0c079ab042547e0ac
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-26 13:28:01 +0300

    Yearn School Update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const colors = require('colors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to Mongoose Database
+connectDB();
 
 app.use(
   '/graphql',
