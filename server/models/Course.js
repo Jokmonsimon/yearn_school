4a84376ab161179918597407dd204a517e51d711
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-30 22:47:44 +0300

    New update

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
 const CourseSchema = new mongoose.Schema({
-  name: { type: String },
-  description: { type: String },
-  duration: { type: String },
+  name: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  duration: {
+    type: String,
+  },
   status: {
     type: String,
     enum: ['Pending', 'Approved', 'Declined'],
-  },
-  projectId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Project',
   },
 });
 
