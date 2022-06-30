84991348e72d3ef55f886ce825fba7a180e648da
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-06-30 22:47:52 +0300

    New update

--- server/models/Student.js
+++ server/models/Student.js
@@ -6,7 +6,7 @@ const StudentSchema = new mongoose.Schema({
   lastName: { type: String },
   gender: {
     type: String,
-    enum: ['Male', 'Female'],
+    enum: ['Select Gender', 'Male', 'Female'],
   },
   dateOfBirth: { type: String },
   emmergencyContactName: { type: String },
