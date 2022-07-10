f00c86e3bd1b7675cd9551dd8b89c1320dd6b665
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-07-10 13:50:13 +0300

    Initial setup

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,29 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FiSetting, FiSettings } from 'react-icons/fi';
+import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import {
+  Dashboard,
+  Courses,
+  Instructors,
+  ColorPicker,
+  Editor,
+  Kanban,
+  Schedule,
+  Projects,
+  Students,
+  Orders,
+  Area,
+  Bar,
+  ColorMapping,
+  Financial,
+  Line,
+  Pie,
+  Pyramid,
+  Stacked,
+} from './pages';
 
 import './App.css';
 
@@ -25,10 +47,12 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className="w-52 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar
+              <Sidebar />
             </div>
           ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
           )}
           <div
             className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
@@ -36,37 +60,38 @@ const App = () => {
             }`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-bg navbar w-full">
-              Navbar
+              <Navbar />
             </div>
           </div>
 
           <div>
             <Routes>
               {/* Dashboard */}
-              <Route path="/" element="Yearn" />
-              <Route path="/yearn" element="Yearn" />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
 
               {/* Pages */}
-              <Route path="courses" element="Courses" />
-              <Route path="projects" element="Projects" />
-              <Route path="instructors" element="Instructors" />
-              <Route path="students" element="Students" />
+              <Route path="courses" element={<Courses />} />
+              <Route path="projects" element={<Projects />} />
+              <Route path="instructors" element={<Instructors />} />
+              <Route path="students" element={<Students />} />
+              <Route path="orders" element={<Orders />} />
 
               {/* Apps */}
-              <Route path="kanban" element="Kanban" />
-              <Route path="editor" element="Editor" />
-              <Route path="planning" element="Planning" />
-              <Route path="color-picker" element="ColorPicker" />
+              <Route path="kanban" element={<Kanban />} />
+              <Route path="editor" element={<Editor />} />
+              <Route path="schedule" element={<Schedule />} />
+              <Route path="color-picker" element={<ColorPicker />} />
 
               {/* Charts */}
-              <Route path="line" element="Line" />
-              <Route path="area" element="Area" />
-              <Route path="bar" element="Bar" />
-              <Route path="pie" element="Pie" />
-              <Route path="financial" element="Financial" />
-              <Route path="color-mapping" element="ColorMapping" />
-              <Route path="pyramid" element="Pyramid" />
-              <Route path="stacked" element="Stacked" />
+              <Route path="line" element={<Line />} />
+              <Route path="area" element={<Area />} />
+              <Route path="bar" element={<Bar />} />
+              <Route path="pie" element={<Pie />} />
+              <Route path="financial" element={<Financial />} />
+              <Route path="color-mapping" element={<ColorMapping />} />
+              <Route path="pyramid" element={<Pyramid />} />
+              <Route path="stacked" element={<Stacked />} />
             </Routes>
           </div>
         </div>
