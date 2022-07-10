8f92965a4f785db54d81a67d32e4f81ee08a40f2
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-07-10 11:28:32 +0300

    Inital Setup

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSetting, FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  const activeMenu = true;
+
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: 'blue', borderRadius: '50%' }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-52 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar</div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? 'md:ml-52' : 'flex-2'
+            }`}
+          >
+            <div className="fixed md:static bg-main-bg dark:bg-main-bg navbar w-full">
+              Navbar
+            </div>
+          </div>
+
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element="Yearn" />
+              <Route path="/yearn" element="Yearn" />
+
+              {/* Pages */}
+              <Route path="courses" element="Courses" />
+              <Route path="projects" element="Projects" />
+              <Route path="instructors" element="Instructors" />
+              <Route path="students" element="Students" />
+
+              {/* Apps */}
+              <Route path="kanban" element="Kanban" />
+              <Route path="editor" element="Editor" />
+              <Route path="planning" element="Planning" />
+              <Route path="color-picker" element="ColorPicker" />
+
+              {/* Charts */}
+              <Route path="line" element="Line" />
+              <Route path="area" element="Area" />
+              <Route path="bar" element="Bar" />
+              <Route path="pie" element="Pie" />
+              <Route path="financial" element="Financial" />
+              <Route path="color-mapping" element="ColorMapping" />
+              <Route path="pyramid" element="Pyramid" />
+              <Route path="stacked" element="Stacked" />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
