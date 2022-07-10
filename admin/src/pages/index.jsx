4fd751b7851dfb61918a7b0a16a1d6b8f78747a5
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-07-10 12:40:11 +0300

    Files and Folders Setup

---
+++ admin/src/pages/index.jsx
@@ -0,0 +1,17 @@
+export { default as Dasboard } from './Dashboard';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Courses } from './Courses';
+export { default as Editor } from './Editor';
+export { default as Instructors } from './Instructors';
+export { default as Kanban } from './Kanban';
+export { default as Schedule } from './Schedule';
+export { default as Projects } from './Projects';
+export { default as Students } from './Students';
+export { default as Area } from './Charts/Area';
+export { default as Bar } from './Charts/Bar';
+export { default as ColorMapping } from './Charts/ColorMapping';
+export { default as Financial } from './Charts/Financial';
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+export { default as Pyramid } from './Charts/Pyramid';
+export { default as Stacked } from './Charts/Stacked';
